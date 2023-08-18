a2e2ddea16cd649755813ef91bfdba26a2610d53
Jay Kothari <jaykothari.d@gmail.com>
2023-08-18 09:58:35 -0400

    collects organization and role data

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
-use clerk_rs::{clerk::Clerk, endpoints::ClerkGetEndpoint, ClerkConfiguration};
+use clerk_rs::{
+    apis::organization_memberships_api::OragnizationMebership, apis::users_api::User, clerk::Clerk,
+    endpoints::ClerkGetEndpoint, models::organization_membership::Role, ClerkConfiguration,
+};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::env;
@@ -18,20 +21,109 @@ pgrx::pg_module_magic!();
 pub struct ClerkFdw {
     row_cnt: i64,
     tgt_cols: Vec<Column>,
-    users: Vec<User>,
-    // organization: Option<String>,
+    users: Vec<UserInfo>,
 }
 
-fn fetch_users(api_key: &str) -> Vec<User> {
+struct UserInfo {
+    id: String,
+    first_name: Option<String>,
+    last_name: Option<String>,
+    email_addresses: String, // ideally it would be a Vec<String>
+    gender: Option<String>,
+    created_at: i64,
+    updated_at: i64,
+    last_sign_in_at: Option<i64>,
+    phone_numbers: Vec<String>,
+    username: Option<String>,
+    organization_names: String, // ideally it would be a Vec<String>
+    organization_roles: String, // ideally it would be a Vec<String>
+                                // organizations_count: i64,
+}
+
+fn role_to_string(role: &Role) -> String {
+    match role {
+        Role::Admin => "Admin".to_string(),
+        Role::BasicMember => "BasicMember".to_string(),
+    }
+}
+
+fn fetch_users(api_key: &str) -> Vec<UserInfo> {
     let rt = Runtime::new().unwrap();
+    let mut user_info_list: Vec<UserInfo> = Vec::new();
+
     rt.block_on(async {
         let clerk_dev_api_token = api_key;
         let config =
             ClerkConfiguration::new(None, None, Some(clerk_dev_api_token.to_string()), None);
         let client = Clerk::new(config);
         let res = client.get(ClerkGetEndpoint::GetUserList).await.unwrap();
-        serde_json::from_value(res).unwrap()
-    })
+        let json_data: Vec<Person> = serde_json::from_value(res)
+            .map_err(|err| {
+                eprintln!("{err}");
+                err
+            })
+            .unwrap();
+
+        for user in json_data {
+            let org_data =
+                User::users_get_organization_memberships(&client, &user.id, Some(0.0), Some(0.0))
+                    .await
+                    .unwrap();
+            let mut organization_names = Vec::new();
+            let mut organization_roles = Vec::new();
+            for membership in org_data.data {
+                if let Some(organization) = membership.organization {
+                    organization_names.push(organization.name);
+
+                    // get data about the roles of the user in the organization
+                    let org_membership_list = OragnizationMebership::list_organization_memberships(
+                        &client,
+                        &organization.id,
+                        Some(0.0),
+                        Some(0.0),
+                    )
+                    .await
+                    .unwrap();
+
+                    // Iterate through the organization memberships to obtain the role
+                    for org_mem in org_membership_list.data {
+                        // Check if the user ID in org_mem matches the current user's ID
+                        if let Some(public_user_data) = org_mem.public_user_data {
+                            if let Some(org_user_id) = public_user_data.user_id {
+                                if org_user_id == user.id {
+                                    if let Some(role) = org_mem.role {
+                                        organization_roles.push(role_to_string(&role));
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            let user_info = UserInfo {
+                id: user.id,
+                first_name: user.first_name,
+                last_name: user.last_name,
+                email_addresses: user
+                    .email_addresses
+                    .first()
+                    .map(|email| email.email_address.clone())
+                    .unwrap_or_default(),
+                gender: user.gender,
+                created_at: user.created_at as i64,
+                updated_at: user.updated_at as i64,
+                last_sign_in_at: user.last_sign_in_at.map(|ts| ts as i64),
+                phone_numbers: user.phone_numbers,
+                username: user.username,
+                organization_names: organization_names.join(","),
+                organization_roles: organization_roles.join(","),
+                // organizations_count: org_data.total_count as i64,
+            };
+            user_info_list.push(user_info);
+        }
+    });
+    return user_info_list;
 }
 
 impl ForeignDataWrapper for ClerkFdw {
@@ -43,12 +135,10 @@ impl ForeignDataWrapper for ClerkFdw {
                 .map(|api_key| fetch_users(&api_key)),
         }
         .unwrap();
-        // let organization = Some("tembo".to_string());
         Self {
             row_cnt: 0,
             tgt_cols: Vec::new(),
             users,
-            // organization,
         }
     }
 
@@ -73,15 +163,7 @@ impl ForeignDataWrapper for ClerkFdw {
                         row.push("first_name", user.first_name.clone().map(Cell::String))
                     }
                     "last_name" => row.push("last_name", user.last_name.clone().map(Cell::String)),
-                    "email" => row.push(
-                        "email",
-                        Some(Cell::String(
-                            user.email_addresses
-                                .first()
-                                .map(|email| email.email_address.clone())
-                                .unwrap_or_default(),
-                        )),
-                    ),
+                    "email" => row.push("email", Some(Cell::String(user.email_addresses.clone()))),
                     "gender" => row.push("gender", user.gender.clone().map(Cell::String)),
                     "created_at" => row.push("created_at", Some(Cell::I64(user.created_at as i64))),
                     "updated_at" => row.push("updated_at", Some(Cell::I64(user.updated_at as i64))),
@@ -96,9 +178,15 @@ impl ForeignDataWrapper for ClerkFdw {
                             .map(|phone| Cell::String(phone.clone())),
                     ),
                     "username" => row.push("username", user.username.clone().map(Cell::String)),
-                    // "organization" => {
-                    //     row.push("organization", self.organization.clone().map(Cell::String))
-                    // }
+                    "organization" => row.push(
+                        "organization",
+                        Some(Cell::String(user.organization_names.clone())),
+                    ),
+                    "role" => row.push("role", Some(Cell::String(user.organization_roles.clone()))),
+                    // "organizations_count" => row.push(
+                    //     "organizations_count",
+                    //     Some(Cell::I64(user.organizations_count)),
+                    // ),
                     _ => {}
                 }
             }
@@ -114,7 +202,7 @@ impl ForeignDataWrapper for ClerkFdw {
 }
 
 #[derive(Debug, Deserialize, Serialize)]
-struct User {
+struct Person {
     backup_code_enabled: bool,
     banned: bool,
     birthday: Option<String>,
