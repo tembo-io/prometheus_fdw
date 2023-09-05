06f938b3b6267c5a9a3cf6cf4e4f61bbe90ded36
Jay Kothari <jaykothari.d@gmail.com>
2023-09-05 09:25:20 -0400

    updated clerk_version and description

--- src/lib.rs
+++ src/lib.rs
@@ -148,7 +148,7 @@ fn resp_to_rows(obj: &str, resp: &JsonValue, tgt_cols: &[Column]) -> Vec<Row> {
 }
 
 #[wrappers_fdw(
-    version = "0.2.3",
+    version = "0.2.4",
     author = "Jay Kothari",
     website = "https://tembo.io"
 )]
