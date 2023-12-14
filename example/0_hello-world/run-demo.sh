18e3cc780f6b4eb8d539e775c148090d7a8e08cc
Steven Miller <sjmiller609@gmail.com>
2023-12-14 11:00:55 -0500

    Adjust to valid query

--- example/0_hello-world/run-demo.sh
+++ example/0_hello-world/run-demo.sh
@@ -12,4 +12,7 @@ echo "Ready!"
 
 # Run sample scripts
 psql postgres://postgres:postgres@localhost:5432 -f ./setup-prometheus-fdw.sql
-psql postgres://postgres:postgres@localhost:5432 -f ./sample-query.sql
+
+start_time=$(date -j -v-500S +%s)
+end_time=$(date -j +%s)
+psql postgres://postgres:postgres@localhost:5432 -v start_time=$start_time -v end_time=$end_time -f ./sample-query.sql
--- example/0_hello-world/sample-query.sql
+++ example/0_hello-world/sample-query.sql
@@ -3,5 +3,5 @@ SELECT
 FROM metrics
 WHERE
   metric_name = 'container_cpu_usage_seconds_total'
-  AND metric_time > EXTRACT(EPOCH FROM NOW() - INTERVAL '500 seconds')
-  AND metric_time < EXTRACT(EPOCH FROM NOW());
+  AND metric_time > :start_time
+  AND metric_time < :end_time;
